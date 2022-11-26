be6bbbe14b8b6fbed93f22bafaba33942972e715
aimeetacchi <aimeetacchi@hotmail.co.uk>
2022-11-26 22:25:28 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import './App.css';
 
-function App() {
+import Nav from './components/nav'
+import Home from './components/pages/home';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className="app-boilerplate">
+      <Nav />
+        <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
     </div>
+    </BrowserRouter>
   );
 }
 
